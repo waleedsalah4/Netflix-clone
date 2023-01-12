2c3271d5dd0dbe61c9ebbf92bddb48912252d4c2
waleed salah <walidsalah652@gmail.com>
2023-01-12 17:10:28 +0200

    add movie sections and finish home page

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+import { Movie } from '../typings'
+
+interface Props {
+    //when using firebase
+    // movie: Movie | DocumentData
+    movie: Movie 
+}
+
+function Thumbnail({movie}: Props) {
+    return (
+        <div 
+        // className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+        className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+        >
+            <Image
+                src={`https://image.tmdb.org/t/p/w500${
+                movie.backdrop_path || movie.poster_path
+                }`}
+                fill
+                // sizes="7rem 180px,
+                // (min-width: 768px) 9rem,
+                // 260px"
+                className="rounded-sm object-cover md:rounded"
+                alt='movie thumbnail'
+            />
+        </div>
+    )
+}
+
+export default Thumbnail
